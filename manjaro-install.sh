b06d7a907eec2d0acb6fab059cd4cbcfdb95b358
Marco Pacchialat <m.pacchialat@gmail.com>
2019-04-29 12:05:25 +0200

    modified install script

--- manjaro-install.sh
+++ manjaro-install.sh
@@ -1,6 +1,16 @@
 sudo pacman -Syu --noconfirm
 
-sudo pacman -S pacaur cmus elinks msmtp neomutt offlineimap python2 python2-pip python3 python3-pip tmux zsh libxml2 libxslt zlib
+sudo pacman -S --noconfirm pacaur python2 python2-pip python3 python3-pip libxml2 libxslt zlib \
+    powerline-fonts tmux zsh ttf-fira-code  \
+    elinks msmtp neomutt offlineimap \
+    clementine chromium \
+    keepassxc ksshaskpass pass \
+
+sudo pacaur -S --noconfirm mozk ibus-mozk otf-stix \
+    dropbox megasync pcloud-drive \
+
+# install Oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 mkdir ~/.virtualenvs
 python3 -m pip install --user -U virtualenv
@@ -13,9 +23,6 @@ virtualenv -p python3 ~/.virtualenvs/vdirsyncer
 ~/.virtualenvs/vdircyncer/pip install vdirsyncer
 ln -s ~/.virtualenvs/vdircyncer/bin/vdirsyncer ~/.virtualenvs/bin 
 
-# install Oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 # copy the necessary files to the home directory
 mkdir ~/Mail
 mkdir ~/Mail/Gmail
@@ -24,3 +31,5 @@ mkdir ~/.msmtp.queue
 mkdir ~/.contacts
 cp -a ./redir/. ~/
 chmod 700 ~/dotfiles/scripts/startmuttintmux.sh
+
+sudo chsh -s /usr/bin/zsh pakky
