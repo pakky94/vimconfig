23915e794998e523fb688d048bed63fce7262e85
unknown <m.pacchialat@gmail.com>
2019-08-12 10:18:30 +0200

    updates to vimrc and wsl install script

--- wsl-ubuntu-install.sh
+++ wsl-ubuntu-install.sh
@@ -1,9 +1,8 @@
 sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install neovim
-sudo apt-get install python-dev python-pip python3-dev python3-pip
+sudo apt-get install neovim python-dev python-pip python3-dev python3-pip
 pip install --user pynvim
-pip3 install --user black pynvim
+pip3 install --user pynvim
 
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install -y nodejs
@@ -11,3 +10,5 @@ sudo npm install -g neovim
 
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb
 sudo dpkg -i ripgrep_11.0.2_amd64.deb
+
+nvim +PlugInstall +UpdateRemotePlugins +qall
